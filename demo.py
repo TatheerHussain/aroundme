764b20657d8b25601e408ece41700736fea19d58
tatheerhussain <tatheerhussain@outlook.com>
2021-01-17 07:43:24 +0800

    demo.py

---
+++ demo.py
@@ -0,0 +1,27 @@
+import requests
+import json
+from data_seacher import *
+import random
+
+#question = input('qustion:')
+#passage = input('passage:')
+def qanet(question, passage):
+        payload = {"question": question, "passage": passage}
+        r = requests.post("http://localhost:5000/answer", data= json.dumps( payload ) , headers = {'content-type': 'application/json'} )
+        answer = json.loads(r.text)['answer']
+        return answer
+
+def main():
+        try:
+                finder = Seacher()
+                question = 'where is Guizikeng Hiking Trail' #'What is the address of Zhinan Temple?'
+                print(question)
+                annoy_list = finder.searchArticle(question)
+                article = random.choice(annoy_list)
+                print(article['url'])
+                print("QANet say:", qanet(question, article['content']))
+        except TypeError:
+                      print("Error")
+
+main()
+
